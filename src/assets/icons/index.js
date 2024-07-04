e0241a2c70d1ed3affbf407a02388b91b9941614
Widya Amala <widyaamala@inovasi360.com>
2024-07-04 22:22:13 +0700

    imp(assets): update assets music dan icons

---
+++ src/assets/icons/index.js
@@ -0,0 +1,55 @@
+import AlarmIcon from "./Alarm.svg"
+import AlarmActiveIcon from "./AlarmActive.svg"
+import ArrowBackIcon from "./ArrowBack.svg"
+import BadgeIcon from "./Badge.svg"
+import CalendarMonthIcon from "./CalendarMonth.svg"
+import ContactPhoneIcon from "./ContactPhone.svg"
+import DeleteIcon from "./Delete.svg"
+import EmailIcon from "./Email.svg"
+import ErrorIcon from "./Error.svg"
+import LabelIcon from "./Label.svg"
+import LibraryMusicIcon from "./LibraryMusic.svg"
+import LibraryMusicActiveIcon from "./LibraryMusicActive.svg"
+import MoreVertIcon from "./MoreVert.svg"
+import NewspaperIcon from "./Newspaper.svg"
+import NewspaperActiveIcon from "./NewspaperActive.svg"
+import PauseCircleIcon from "./PauseCircle.svg"
+import PlayCircleIcon from "./PlayCircle.svg"
+import RepeatIcon from "./Repeat.svg"
+import RepeatOneIcon from "./RepeatOne.svg"
+import RepeatOnIcon from "./RepeatOn.svg"
+import SkipNextIcon from "./SkipNext.svg"
+import SkipPreviousIcon from "./SkipPrevious.svg"
+import Visibility from "./Visibility.svg"
+import VisibilityOff from "./VisibilityOff.svg"
+import VolumeUpIcon from "./VolumeUp.svg"
+import VolumeOffIcon from "./VolumeOff.svg"
+
+export {
+  AlarmIcon,
+  AlarmActiveIcon,
+  ArrowBackIcon,
+  BadgeIcon,
+  CalendarMonthIcon,
+  ContactPhoneIcon,
+  DeleteIcon,
+  EmailIcon,
+  ErrorIcon,
+  LabelIcon,
+  LibraryMusicIcon,
+  LibraryMusicActiveIcon,
+  MoreVertIcon,
+  NewspaperIcon,
+  NewspaperActiveIcon,
+  PauseCircleIcon,
+  PlayCircleIcon,
+  RepeatIcon,
+  RepeatOneIcon,
+  RepeatOnIcon,
+  SkipNextIcon,
+  SkipPreviousIcon,
+  Visibility,
+  VisibilityOff,
+  VolumeUpIcon,
+  VolumeOffIcon
+}
