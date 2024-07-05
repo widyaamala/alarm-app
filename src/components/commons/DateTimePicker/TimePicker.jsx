f3bd9f5de8fd8da3c6f6e6f9c4a725682ca9b704
Widya Amala <widyaamala@inovasi360.com>
2024-07-05 14:19:42 +0700

    imp(component): improvement style component timepicker

--- src/components/commons/DateTimePicker/TimePicker.jsx
+++ src/components/commons/DateTimePicker/TimePicker.jsx
@@ -1,41 +1,58 @@
-import { useState } from 'react';
-import DateTimePicker from 'react-weblineindia-time-picker'
-import "./datetimepicker.css"
+import { useState } from "react";
+import DateTimePicker from "react-weblineindia-time-picker";
+import "./datetimepicker.css";
+import { Button, HStack } from "@chakra-ui/react";
 
 const TimePicker = ({ value, handleChange, toggle }) => {
-  const [tempTime, setTempTime] = useState(value ?? new Date())
+  const [tempTime, setTempTime] = useState(value ?? new Date());
 
   const handleSelect = (time) => {
-    setTempTime(time.value)
-  }
+    setTempTime(time.value);
+  };
 
   const handleOk = () => {
     if (handleChange) {
-      handleChange(tempTime)
+      handleChange(tempTime);
     }
-  }
+  };
 
   const handleCancel = () => {
-    setTempTime(value ?? new Date())
-    if (toggle) toggle()
-  }
+    setTempTime(value ?? new Date());
+    if (toggle) toggle();
+  };
 
   return (
     <div className="overlay-picker">
       <div className="picker">
         <DateTimePicker
-          hourFormat='12'
+          hourFormat="12"
           value={tempTime}
           onChange={handleSelect}
           timeOnly
         />
-        <div className="confirm-picker">
-          <button onClick={handleCancel}>Cancel</button>
-          <button onClick={handleOk}>OK</button>
-        </div>
+        <HStack justifyContent="end" mt="5">
+          <Button
+            onClick={handleCancel}
+            h="8"
+            textStyle="light"
+            fontSize="14px"
+            _focus={{ bg: "white" }}
+          >
+            Cancel
+          </Button>
+          <Button
+            onClick={handleOk}
+            h="8"
+            textStyle="light"
+            fontSize="14px"
+            variant="primary"
+          >
+            OK
+          </Button>
+        </HStack>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default TimePicker
+export default TimePicker;
